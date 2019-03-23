f034d1b3e461ed6880c34b5183934a5766a4543d
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2019-03-23 13:05:17 +0200

    Added some notes about the @WebServlet annotation.

--- src/main/java/net/toiviainen/servlet/sandbox/SandboxHttpServlet.java
+++ src/main/java/net/toiviainen/servlet/sandbox/SandboxHttpServlet.java
@@ -8,8 +8,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+ * @WebServlet annotation attributes.
+ * name.............The name of the servlet.
+ * value............Delegate for urlPatterns.
+ * urlPatterns......The URL patterns of the servlet.
+ * loadOnstartup....Allows specifying the loading order for servlet(s).
+ * initParams.......The array of initialization parameters for the servlet.
+ * asyncSupported...Defines whether servlet supports asynchronous operation mode.
+ * smallIcon........Small icon for the servlet.
+ * largeIcon........Big icon for the servlet.
+ * description......Servlet's description.
+ * displayName......Servlet's display name.
+ */
 @SuppressWarnings("serial")
-@WebServlet(value = "sandbox", loadOnStartup = 1)
+@WebServlet(name = "sandbox servlet name", value = "sandbox", loadOnStartup = 1, asyncSupported = true)
 public class SandboxHttpServlet extends HttpServlet {
 
 	@Override
