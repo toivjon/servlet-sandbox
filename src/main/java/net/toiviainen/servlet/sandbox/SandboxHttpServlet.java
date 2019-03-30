95c11dd7cb94342eaa324fcc902ef7c27a54e073
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2019-03-30 07:50:58 +0200

    Added some printouts to check what kind of data HttpServletMapping contains.

--- src/main/java/net/toiviainen/servlet/sandbox/SandboxHttpServlet.java
+++ src/main/java/net/toiviainen/servlet/sandbox/SandboxHttpServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -25,19 +26,28 @@ import javax.servlet.http.HttpSession;
  * displayName......Servlet's display name.
  */
 @SuppressWarnings("serial")
-@WebServlet(name = "sandbox servlet name", value = "/*", loadOnStartup = 1, asyncSupported = true)
+@WebServlet(name = "sandbox servlet name", value = "/", loadOnStartup = 1, asyncSupported = true)
 public class SandboxHttpServlet extends HttpServlet {
+
+	// http://alibassam.com/everything-new-servlet-4-0/
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		ServletContext ctx = getServletContext();
+		System.out.println("SandboxHttpServlet#doGet");
+
+		// Servlet 4.0: get information related how request was mapped to servlet.
+		HttpServletMapping servletMapping = req.getHttpServletMapping();
+		System.out.println("HttpServletMapping: ");
+		System.out.println("\tmappingMatch : " + servletMapping.getMappingMatch());
+		System.out.println("\tmatchValue   : " + servletMapping.getMatchValue());
+		System.out.println("\tpattern      : " + servletMapping.getPattern());
+		System.out.println("\tservletName  : " + servletMapping.getServletName());
 
 		// debug and refresh HTTP session value each time we arrive.
+		ServletContext ctx = getServletContext();
 		HttpSession session = req.getSession();
 		ctx.log("A value from HTTP session: " + session.getAttribute("foo"));
 		session.setAttribute("foo", Instant.now());
-
-		resp.setStatus(HttpServletResponse.SC_OK);
 	}
 
 }
