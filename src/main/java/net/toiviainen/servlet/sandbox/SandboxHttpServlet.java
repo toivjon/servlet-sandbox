6dd9a8996fd5fa2a1d06b85943e523f51f18a718
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2019-03-31 10:30:58 +0300

    Added a simple server push tests into servlet impl.

--- src/main/java/net/toiviainen/servlet/sandbox/SandboxHttpServlet.java
+++ src/main/java/net/toiviainen/servlet/sandbox/SandboxHttpServlet.java
@@ -1,6 +1,7 @@
 package net.toiviainen.servlet.sandbox;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.Instant;
 
 import javax.servlet.ServletContext;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.PushBuilder;
 
 /*
  * @WebServlet annotation attributes.
@@ -29,23 +31,43 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "sandbox servlet name", value = "/", loadOnStartup = 1, asyncSupported = true)
 public class SandboxHttpServlet extends HttpServlet {
 
+	private static final String LOG_PREFIX = String.format("[%s] ", SandboxHttpServlet.class.getSimpleName());
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("SandboxHttpServlet#doGet");
-
 		// Servlet 4.0: get information related how request was mapped to servlet.
 		HttpServletMapping servletMapping = req.getHttpServletMapping();
-		System.out.println("HttpServletMapping: ");
-		System.out.println("\tmappingMatch : " + servletMapping.getMappingMatch());
-		System.out.println("\tmatchValue   : " + servletMapping.getMatchValue());
-		System.out.println("\tpattern      : " + servletMapping.getPattern());
-		System.out.println("\tservletName  : " + servletMapping.getServletName());
+		System.out.println(LOG_PREFIX + "HttpServletMapping: ");
+		System.out.println(LOG_PREFIX + "\tmappingMatch : " + servletMapping.getMappingMatch());
+		System.out.println(LOG_PREFIX + "\tmatchValue   : " + servletMapping.getMatchValue());
+		System.out.println(LOG_PREFIX + "\tpattern      : " + servletMapping.getPattern());
+		System.out.println(LOG_PREFIX + "\tservletName  : " + servletMapping.getServletName());
 
-		// debug and refresh HTTP session value each time we arrive.
-		ServletContext ctx = getServletContext();
-		HttpSession session = req.getSession();
-		ctx.log("A value from HTTP session: " + session.getAttribute("foo"));
-		session.setAttribute("foo", Instant.now());
+		// try to build a push builder so we can pro-actively send resources.
+		PushBuilder pushBuilder = req.newPushBuilder();
+		boolean pushEnabled = (pushBuilder != null);
+		System.out.println(LOG_PREFIX + "Server push enabled: " + pushEnabled);
+		if (pushEnabled) {
+			pushBuilder.path("image.png");
+			pushBuilder.push();
+		}
+
+		// build a web page to be shown to user.
+		StringBuilder builder = new StringBuilder();
+		builder.append("<!DOCTYPE html>");
+		builder.append("<html>");
+		builder.append("<head>");
+		builder.append("<title>Servlet 4.0 Sandbox</title>");
+		builder.append("</head>");
+		builder.append("<body>");
+		builder.append("<h1>You should see an image right below this text</h1>");
+		builder.append("<img src='/servlet-sandbox/image.png' alt='Image'/>");
+		builder.append("</body>");
+		builder.append("</html>");
+		try (PrintWriter writer = resp.getWriter()) {
+			writer.write(builder.toString());
+		}
+		resp.setStatus(HttpServletResponse.SC_OK);
 	}
 
 }
